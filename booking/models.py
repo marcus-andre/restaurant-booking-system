82298c9b892cb15fe96d7db34e1721ffaa50782e
marcus-andre <marcus-andre@hotmail.com>
2025-12-27 15:49:57 -0300

    Fix: Add RegexValidator to phone field to prevent non-numeric input (AC7)

--- booking/models.py
+++ booking/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import RegexValidator  # import validator
 
 # Create your models here.
 
@@ -11,7 +12,13 @@ class Booking(models.Model):
         User, on_delete=models.CASCADE, related_name="bookings")
     name = models.CharField(max_length=100)
     email = models.EmailField()
-    phone = models.CharField(max_length=15)
+
+    phone_regex = RegexValidator(
+        regex=r'^\+?1?\d{9,15}$',
+        message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed."
+    )
+
+    phone = models.CharField(max_length=17)
     booking_date = models.DateField()
     booking_time = models.TimeField()
     number_of_guests = models.IntegerField(default=1)
